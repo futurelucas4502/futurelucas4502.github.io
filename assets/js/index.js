8458ea21da18f67b3ef2fb8f44aef9a999f745b2
Lucas Wilson <lucaswilson4502@outlook.com>
2020-06-01 09:40:39 +0100

    test idea for styling readme's better

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,70 +1,74 @@
 import { site_url, owner, useAPI, fixedName1, fixedName2, fixedName1FA, fixedName2FA, full_name } from "./setup.js"
 
-// Start code (Don't edit data below unless you need to)
+function get(name) {
+  if (name = (new RegExp('[?&]' + encodeURIComponent(name) + '=([^&]*)')).exec(location.search))
+    return decodeURIComponent(name[1]);
+}
+
 // Start Navigation
 var mobilehref
 var converter = new showdown.Converter();
 var closeNavMobile = window.matchMedia("(max-width: 991px)")
 closeNavMobile.addListener(closeNavMobileFunc) // Attach listener function on state changes
 
-function closeNavMobileFunc(){
-    $('.navbar-collapse').collapse('hide');
+function closeNavMobileFunc() {
+  $('.navbar-collapse').collapse('hide');
 }
 
-function init(href){ // Initial animation of hori-selector
-  if(href != undefined){
+function init(href) { // Initial animation of hori-selector
+  if (href != undefined) {
     $('#navbarSupportedContent ul li').removeClass("active");
-    if($('#navbarSupportedContent ul li a[href="'+decodeURI(href)+'"]').parent()[0].id == "navDropdownInner"){
-      $('#navbarSupportedContent ul li a[href="'+decodeURI(href)+'"]').parent().parent().addClass("active")
+    if ($('#navbarSupportedContent ul li a[href="' + decodeURI(href) + '"]').parent()[0].id == "navDropdownInner") {
+      $('#navbarSupportedContent ul li a[href="' + decodeURI(href) + '"]').parent().parent().addClass("active")
     } else {
-      $('#navbarSupportedContent ul li a[href="'+decodeURI(href)+'"]').parent().addClass("active")
+      $('#navbarSupportedContent ul li a[href="' + decodeURI(href) + '"]').parent().addClass("active")
     }
   }
-  if(mobilehref != undefined){
+  if (mobilehref != undefined) {
     $('#navbarSupportedContent ul li').removeClass("active");
-    if($('#navbarSupportedContent ul li a[href="'+decodeURI(mobilehref)+'"]').parent()[0].id == "navDropdownInner"){
-      $('#navbarSupportedContent ul li a[href="'+decodeURI(mobilehref)+'"]').parent().parent().addClass("active")
+    if ($('#navbarSupportedContent ul li a[href="' + decodeURI(mobilehref) + '"]').parent()[0].id == "navDropdownInner") {
+      $('#navbarSupportedContent ul li a[href="' + decodeURI(mobilehref) + '"]').parent().parent().addClass("active")
     } else {
-      $('#navbarSupportedContent ul li a[href="'+decodeURI(mobilehref)+'"]').parent().addClass("active")
+      $('#navbarSupportedContent ul li a[href="' + decodeURI(mobilehref) + '"]').parent().addClass("active")
     }
     mobilehref = undefined
   }
-    var tabsNewAnim = $('#navbarSupportedContent');
-    var activeItemNewAnim = tabsNewAnim.find('.active');
-    var activeWidthNewAnimHeight = activeItemNewAnim.innerHeight();
-    var activeWidthNewAnimWidth = activeItemNewAnim.innerWidth();
-    var itemPosNewAnimTop = activeItemNewAnim.position();
-    var itemPosNewAnimLeft = activeItemNewAnim.position();
-    if(closeNavMobile.matches){ // Mobile view
-      $(".hori-selector").css({
-        "top":itemPosNewAnimTop.top + "px", 
-        "left":itemPosNewAnimLeft.left + "px",
-        "height": activeWidthNewAnimHeight + "px",
-        "width": activeWidthNewAnimWidth + "px"
-      });
-      setTimeout(function(){ mobileNavRightCut(); }, 275)
-    } else { // Desktop view
-      $(".hori-selector").css({
-        "top":itemPosNewAnimTop.top + "px", 
-        "left":itemPosNewAnimLeft.left + "px",
-        "height": (activeWidthNewAnimHeight - 10) + "px",
-        "width": activeWidthNewAnimWidth + "px"
-      });
-    }
-  };
+  var tabsNewAnim = $('#navbarSupportedContent');
+  var activeItemNewAnim = tabsNewAnim.find('.active');
+  var activeWidthNewAnimHeight = activeItemNewAnim.innerHeight();
+  var activeWidthNewAnimWidth = activeItemNewAnim.innerWidth();
+  var itemPosNewAnimTop = activeItemNewAnim.position();
+  var itemPosNewAnimLeft = activeItemNewAnim.position();
+  if (closeNavMobile.matches) { // Mobile view
+    $(".hori-selector").css({
+      "top": itemPosNewAnimTop.top + "px",
+      "left": itemPosNewAnimLeft.left + "px",
+      "height": activeWidthNewAnimHeight + "px",
+      "width": activeWidthNewAnimWidth + "px"
+    });
+    setTimeout(function () { mobileNavRightCut(); }, 275)
+  } else { // Desktop view
+    $(".hori-selector").css({
+      "top": itemPosNewAnimTop.top + "px",
+      "left": itemPosNewAnimLeft.left + "px",
+      "height": (activeWidthNewAnimHeight - 10) + "px",
+      "width": activeWidthNewAnimWidth + "px"
+    });
+  }
+};
 
-  $(window).on('resize', function(){
-    setTimeout(function(){ init(); }, 500); // Runs init when page resized to ensure its in the correct place
-  });
-  $(".navbar-toggler").click(function(){
-      console.log("toggler clicked")
-      setTimeout(function(){ init(); }, 200);
-  });
+$(window).on('resize', function () {
+  setTimeout(function () { init(); }, 500); // Runs init when page resized to ensure its in the correct place
+});
+$(".navbar-toggler").click(function () {
+  console.log("toggler clicked")
+  setTimeout(function () { init(); }, 200);
+});
 
-  $(function() {
-    if(Modernizr.history){ // Check if history pushing is enabled in this browser its 2020 so this probably isnt nessecary but just in case
-    $("nav").delegate("a", "click", function(e) { // The code that executes when link pressed
-      if (getComputedStyle(document.getElementById("toggler"), null).display != "none" && this.id != "navbarDropdown"){
+$(function () {
+  if (Modernizr.history) { // Check if history pushing is enabled in this browser its 2020 so this probably isnt nessecary but just in case
+    $("nav").delegate("a", "click", function (e) { // The code that executes when link pressed
+      if (getComputedStyle(document.getElementById("toggler"), null).display != "none" && this.id != "navbarDropdown") {
         $('.navbar-collapse').collapse('hide'); // Closes nav toggler when a link is pressed in mobile view
       }
       e.preventDefault(); // Cancel redirection to prevent page loading like normal
@@ -72,52 +76,52 @@ function init(href){ // Initial animation of hori-selector
         var _link = ($(this).attr("href")).trim();
         history.pushState(null, null, _link); // Add link to browser history
         loadContent(_link); // Run custom load instead of redirect
-      } catch(e) {
+      } catch (e) {
         console.log(e)
       }
     });
 
     function loadContent(href) {
-      if(href == "index.html"){
+      if (href == "index.html") {
         indexReady()
-      }else {
+      } else {
         otherReady(href.split("=")[1])
       }
-      if (getComputedStyle(document.getElementById("toggler"), null).display == "none"){
-        setTimeout(function(){init(href)}, 600); // Set timeout instead of loading immediatly to allow for DOM delays e.g. loading of scroll bar
+      if (getComputedStyle(document.getElementById("toggler"), null).display == "none") {
+        setTimeout(function () { init(href) }, 600); // Set timeout instead of loading immediatly to allow for DOM delays e.g. loading of scroll bar
       } else {
         mobilehref = href
       }
     };
-    
-    $(window).bind('popstate', function(){ // Run on forward or back pressed
+
+    $(window).bind('popstate', function () { // Run on forward or back pressed
       _link = location.pathname.replace(/^.*[\\\/]/, '') // Get filename only of the history link
-      if(location.href.split("?")[1] != undefined)_link+=("?"+location.href.split("?")[1]); // Add the page request
-      if(_link == "" || _link == "index"){
+      if (location.href.split("?")[1] != undefined) _link += ("?" + location.href.split("?")[1]); // Add the page request
+      if (_link == "" || _link == "index") {
         _link = "index.html" // Adds support for going back to just site_URL
       }
-      if (!(getComputedStyle(document.getElementById("toggler"), null).display == "none")){
+      if (!(getComputedStyle(document.getElementById("toggler"), null).display == "none")) {
         $('.navbar-collapse').collapse('hide'); // Closes nav toggler before loading past or future content
       }
       loadContent(_link); // Runs the load content function using the history href/_link
     });
 
-} // else {History is not supported, so nothing fancy here.}
+  } // else {History is not supported, so nothing fancy here.}
 });
 $(document).ready(function () { // Closes nav toggler if opened on mobile and you dont press on it
   $(document).click(function (event) {
-      var clickover = $(event.target);
-      var _opened = !$(".navbar-toggler").hasClass("collapsed");
-      if (_opened === true && !clickover.parent().hasClass("navbar-toggler")){
-        document.getElementById("toggler").click()
-      }
+    var clickover = $(event.target);
+    var _opened = !$(".navbar-toggler").hasClass("collapsed");
+    if (_opened === true && !clickover.parent().hasClass("navbar-toggler")) {
+      document.getElementById("toggler").click()
+    }
   });
 });
 
-function mobileNavRightCut(){
-  if($('#navbarSupportedContent').find('.active')[0].children[0].id == "navbarDropdown"){
+function mobileNavRightCut() {
+  if ($('#navbarSupportedContent').find('.active')[0].children[0].id == "navbarDropdown") {
     document.getElementById("right").style.display = "none"
-  } else{
+  } else {
     document.getElementById("right").style.display = "block"
   }
 }
@@ -137,30 +141,30 @@ $(document).ready(async function () {
   fixedName1Var = fixedName1Var.replace(/(^\w{1})|(\s{1}\w{1})/g, match => match.toUpperCase());
   var fixedName2Var = fixedName2.replace(/_/g, ' ');
   fixedName2Var = fixedName2Var.replace(/(^\w{1})|(\s{1}\w{1})/g, match => match.toUpperCase());
-  if(useAPI){
+  if (useAPI) {
     indexResponse = await fetch(`https://api.github.com/users/${owner}/repos`); // Using github API
     indexResponse = await indexResponse.json();
-  } else{
-    await fetch('https://cors-anywhere.herokuapp.com/'+`https://github.com/${owner}?tab=repositories`).then(res => { // Not using github API
-    // The API call was successful!
-    return res.text();
-  }).then(function (html) {
-  
-    // Convert the HTML string into a document object
-    var parser = new DOMParser();
-    var doc = parser.parseFromString(html, 'text/html');
-    // Get the data
-    for (let i = 0; i < doc.querySelectorAll("a[itemprop='name codeRepository']").length; i++) {
-      let tempName = {
-        "name" : doc.querySelectorAll("a[itemprop='name codeRepository']")[i].innerText.replace(/\s+/g, ''),
-        "description" : doc.querySelectorAll("p[itemprop='description']")[i].innerText,
-        "html_url" : `https://github.com/${owner}/${(doc.querySelectorAll("a[itemprop='name codeRepository']")[i].innerText).replace(/\s+/g, '')}`
+  } else {
+    await fetch('https://cors-anywhere.herokuapp.com/' + `https://github.com/${owner}?tab=repositories`).then(res => { // Not using github API
+      // The API call was successful!
+      return res.text();
+    }).then(function (html) {
+
+      // Convert the HTML string into a document object
+      var parser = new DOMParser();
+      var doc = parser.parseFromString(html, 'text/html');
+      // Get the data
+      for (let i = 0; i < doc.querySelectorAll("a[itemprop='name codeRepository']").length; i++) {
+        let tempName = {
+          "name": doc.querySelectorAll("a[itemprop='name codeRepository']")[i].innerText.replace(/\s+/g, ''),
+          "description": doc.querySelectorAll("p[itemprop='description']")[i].innerText,
+          "html_url": `https://github.com/${owner}/${(doc.querySelectorAll("a[itemprop='name codeRepository']")[i].innerText).replace(/\s+/g, '')}`
+        }
+        indexResponse.push(tempName)
       }
-      indexResponse.push(tempName)
-    }
-  })
+    })
   }
-  if(document.location.href == `${site_url}/index.html` || document.location.href == `${site_url}/index` || document.location.href == `${site_url}/` || document.location.href == "http://localhost/futurelucas4502.github.io/index.html" || document.location.href == "http://localhost/futurelucas4502.github.io/index" || document.location.href == "http://localhost/futurelucas4502.github.io/"){
+  if (document.location.href == `${site_url}/index.html` || document.location.href == `${site_url}/index` || document.location.href == `${site_url}/` || document.location.href == "http://localhost/futurelucas4502.github.io/index.html" || document.location.href == "http://localhost/futurelucas4502.github.io/index" || document.location.href == "http://localhost/futurelucas4502.github.io/") {
     indexReady()
   } else {
     otherReady(location.href.split("=")[1])
@@ -168,25 +172,25 @@ $(document).ready(async function () {
   for (let i = 0; i < indexResponse.length; i++) { // Add links to dropdown
     var name = indexResponse[i]["name"].replace(/_/g, ' ');
     name = name.replace(/(^\w{1})|(\s{1}\w{1})/g, match => match.toUpperCase());
-    if(name == fixedName1Var){
+    if (name == fixedName1Var) {
       document.getElementById("fixedName1").innerHTML = `<i class="far ${fixedName1FA}"></i>${fixedName1Var}`
       document.getElementById("fixedName1").href = `index.html?page=${indexResponse[i]["name"]}`
-    }else if(name == fixedName2Var){
+    } else if (name == fixedName2Var) {
       document.getElementById("fixedName2").innerHTML = `<i class="far ${fixedName2FA}"></i>${fixedName2Var}`
       document.getElementById("fixedName2").href = `index.html?page=${indexResponse[i]["name"]}`
-    }else {
+    } else {
       document.getElementById("navDropdownInner").innerHTML += `<a href="index.html?page=${indexResponse[i]["name"]}" class="dropdown-item">${name}</a>`
     }
   }
-  if(location.href.split("?")[1] != undefined){
+  if (location.href.split("?")[1] != undefined) {
     $('#navbarSupportedContent ul li').removeClass("active");
-    if($('#navbarSupportedContent ul li a[href="'+decodeURI("index.html?"+location.href.split("?")[1])+'"]').parent()[0].id == "navDropdownInner"){
-      $('#navbarSupportedContent ul li a[href="'+decodeURI("index.html?"+location.href.split("?")[1])+'"]').parent().parent().addClass("active")
+    if ($('#navbarSupportedContent ul li a[href="' + decodeURI("index.html?" + location.href.split("?")[1]) + '"]').parent()[0].id == "navDropdownInner") {
+      $('#navbarSupportedContent ul li a[href="' + decodeURI("index.html?" + location.href.split("?")[1]) + '"]').parent().parent().addClass("active")
     } else {
-      $('#navbarSupportedContent ul li a[href="'+decodeURI("index.html?"+location.href.split("?")[1])+'"]').parent().addClass("active")
+      $('#navbarSupportedContent ul li a[href="' + decodeURI("index.html?" + location.href.split("?")[1]) + '"]').parent().addClass("active")
     }
   }
-  setTimeout(function(){ init(); }, 700); // Runs init when page first loaded
+  setTimeout(function () { init(); }, 700); // Runs init when page first loaded
 })
 async function indexReady() {
   document.title = `${owner}'s Work`
@@ -234,36 +238,36 @@ async function indexReady() {
 let otherResponse = Array
 async function otherReady(name) {
   document.title = `${owner}'s Work || ${(name.replace(/_/g, ' ')).replace(/(^\w{1})|(\s{1}\w{1})/g, match => match.toUpperCase())}`
-  document.getElementById("main-content").innerHTML = `<div style="margin:20px 0px" class="text-center">
-  <div class="container">
-      <h4 style="margin:20px 0px" class="text-muted" id="loading"><span class="spinner-border m-1"
-              style="width: 1.25rem;height: 1.25rem;border-width: .2rem;" role="status"
-              aria-hidden="true"></span>Loading...</h4>
-      <div id="content">
-  </div>
-  <a style="display: block!important;" href="${site_url}/docs/index.html?docs=${name}" class="btn btn-primary">View Documentation</a>
-  <h6 style="margin:20px 0px" class="text-muted">Disclaimer: ${name} is owned and
-      maintained by ${full_name} (${owner}).</h6>
-</div>`
-var html
-if(useAPI){
-  if(otherResponse[name]==undefined){ // Method using github API which has a rate limit of 60 requests an hour
-    otherResponse[name] = await fetch(`https://api.github.com/repos/${owner}/${name}/readme`);
-    otherResponse[name] = await otherResponse[name].json();
-  }
-  html = converter.makeHtml(atob(otherResponse[name]["content"]));
-} else {
-  if(otherResponse[name]==undefined){ // Method where I fetch the file contents of the readme manually
-    await fetch(`https://raw.githubusercontent.com/${owner}/${name}/master/README.md`).then(res => {
-      return res.text();
-  })
-  .then(data => {
-    otherResponse[name] = data;
+
+  // <a style="display: block!important;" href="${site_url}/docs/index.html?docs=${name}" class="btn btn-primary">View Documentation</a>
+  // <h6 style="margin:20px 0px" class="text-muted">Disclaimer: ${name} is owned and maintained by ${full_name} (${owner}).</h6>
+
+  var html
+  var htmlInner
+  await fetch(`${site_url}/docs/index.html`).then(res => {
+    return res.text()
+  }).then(data => {
+    html = data
   });
+  if (useAPI) {
+    if (otherResponse[name] == undefined) { // Method using github API which has a rate limit of 60 requests an hour
+      otherResponse[name] = await fetch(`https://api.github.com/repos/${owner}/${name}/readme`);
+      otherResponse[name] = await otherResponse[name].json();
+    }
+    htmlInner = converter.makeHtml(atob(otherResponse[name]["content"]));
+  } else {
+    if (otherResponse[name] == undefined) { // Method where I fetch the file contents of the readme manually
+      await fetch(`https://raw.githubusercontent.com/${owner}/${name}/master/README.md`).then(res => {
+        return res.text();
+      }).then(data => {
+        otherResponse[name] = data;
+      });
+    }
+    htmlInner = converter.makeHtml(otherResponse[name]);
   }
-  html = converter.makeHtml(otherResponse[name]);
-}
   document.getElementById("loading").style.display = "none"
-  document.getElementById("content").innerHTML = html
+  document.getElementById("main-content").innerHTML = html
+  console.log(document.getElementById("main-content").innerHTML)
+  document.getElementById("content").innerHTML = htmlInner
 }
 // End Other Pages Loading
