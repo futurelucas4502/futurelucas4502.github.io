f5b957e4b5da19dd53be22c7bd0c8b104a377c64
Lucas Wilson <lucaswilson4502@outlook.com>
2020-06-01 12:23:30 +0100

    added docs as markdown for nice editing

--- assets/js/setup.js
+++ assets/js/setup.js
@@ -16,6 +16,24 @@ var fixedName1FA = "fa-address-book" // Font awesome icon to use for the fixed n
 var fixedName2FA = "fa-address-book" // Font awesome icon to use for the fixed name links
 var full_name = "Lucas Wilson" // Your full name
 var repo_name = "futurelucas4502.github.io" // Name of the repo your website is stored in
+var docs = `
+# futurelucas4502.github.io - Documentation:
+
+Welcome this is a little project I made mostly for myself but the idea is that anyone can fork this and use it on their own github pages or jekyll site (may need extra config for jekyll).
+
+## Setup:
+
+1. Fork the repo
+2. Go into the setting and change the name to your-username.github.io
+3. Open the setup.js file in assets/js/ and go through the comments in the file to change the 8 variables needed
+4. Save and commit your changes to setup.js and your done! (Yes its as easy as that)
+
+**Note**: It may take up to 20 minutes for the site to appear live after dollowing these steps.
+
+## Further customisation:
+
+If you want to host your site at somewhere other than your-username.github.io you can go into the repo settings and set this just make sure you change the site_url in the setup.js file!
+`
 
 
-export { site_url, owner, useAPI, fixedName1, fixedName2, fixedName1FA, fixedName2FA, full_name, repo_name }
+export { site_url, owner, useAPI, fixedName1, fixedName2, fixedName1FA, fixedName2FA, full_name, repo_name, docs }
