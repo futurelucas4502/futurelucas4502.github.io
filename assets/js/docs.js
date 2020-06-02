94a5670faef9f7ca87341b37b4668aed78a05b3f
Lucas Wilson <lucaswilson4502@outlook.com>
2020-06-02 20:32:15 +0100

    moved button style to index.js and docs

--- assets/js/docs.js
+++ assets/js/docs.js
@@ -1,7 +1,3 @@
-document.getElementById("github-view").style.display = "none"
-document.getElementById("download-zip").style.display = "none"
-document.getElementById("download-tar").style.display = "none"
-
 import { site_url, owner, useAPI, cors, repo_name, docs } from "./setup.js"
 var converter = new showdown.Converter();
 var otherResponse = Array()
