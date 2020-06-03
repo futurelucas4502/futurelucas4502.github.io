677c68dab060b15e502b1d283f23380006afdf67
Lucas Wilson <lucaswilson4502@outlook.com>
2020-06-03 13:13:31 +0100

    remove excess code

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,40 +2,16 @@ import { site_url, owner, useAPI, cors, fixedName1, fixedName2, fixedName1FA, fi
 
 // Installable popups start
 
-const divInstall = document.getElementById("installContainer");
-const butInstall = document.getElementById("butInstall");
-
 /* Put code here */
 window.addEventListener("beforeinstallprompt", event => {
   console.log("👍", "beforeinstallprompt", event);
-  // Stash the event so it can be triggered later.
-  window.deferredPrompt = event;
-  // Remove the 'hidden' class from the install button container
-  divInstall.classList.toggle("hidden", false);
-});
-
-butInstall.addEventListener("click", () => {
-  console.log("👍", "butInstall-clicked");
-  const promptEvent = window.deferredPrompt;
-  if (!promptEvent) {
-    // The deferred prompt isn't available.
+  if (!event) {
     return;
   }
-  // Show the install prompt.
-  promptEvent.prompt();
-  // Log the result
-  promptEvent.userChoice.then(result => {
-    console.log("👍", "userChoice", result);
-    // Reset the deferred prompt variable, since
-    // prompt() can only be called once.
-    window.deferredPrompt = null;
-    // Hide the install button.
-    divInstall.classList.toggle("hidden", true);
+  event.prompt();
+  event.userChoice.then(result => {
+    event = null;
   });
-});
-
-window.addEventListener("appinstalled", event => {
-  console.log("👍", "appinstalled", event);
 });
 
 /* Only register a service worker if it's supported */
